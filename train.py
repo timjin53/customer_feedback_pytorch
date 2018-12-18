9f5e88b811eebaadda0b87dc175c323aa6d5c0b9
Tim Jin <tim.jin@telus.com>
2018-12-17 21:02:03 -0500

    updated param and fix GPU run

--- train.py
+++ train.py
@@ -7,11 +7,6 @@ import torch.optim as optim
 from sklearn.model_selection import train_test_split
 from Models.SkipGram import SkipGram 
 
-
-def onehot_encode(indices, vocabulary_size):
-  result = torch.Tensor().new_zeros(len(indices), vocabulary_size)
-  print(result.size())
-
 df = pd.read_csv('data/processed_data/idx_pairs.csv')
 index_pairs = df.values
 vocabulary_size = 11343 #todo dynamic import
@@ -23,8 +18,8 @@ data_test, data_validation = train_test_split(data_test, test_size=0.5, random_s
 # Hyper params
 embedding_size = 300
 num_epochs = 2
-learning_rate = 0.05
-batch_size = 512
+learning_rate = 0.001
+batch_size = 2048
 
 model = SkipGram(vocab_size=vocabulary_size, 
                 embedding_dim=embedding_size)
@@ -38,11 +33,12 @@ print(device)
 data_train = torch.tensor(data_train).to(device)
 # print(len(data_train)) =>795216
 num_batch = int(np.ceil(len(data_train)/batch_size))
+model.to(device)
 
 # batch training
 for epoch in range(num_epochs):
   for batch in range(num_batch):
-    print('epoch: {0}/{1}, batch: {2}/{3}'.format(epoch, num_epochs, batch, num_batch))
+    print('epoch: {0}/{1}, batch: {2}/{3}'.format(epoch+1, num_epochs, batch+1, num_batch))
     batch_loss = 0
     startIndex = batch*batch_size
     x = data_train[startIndex:startIndex+batch_size, 0]
@@ -54,7 +50,7 @@ for epoch in range(num_epochs):
     loss.backward()
     optimizer.step()
     batch_loss += loss.item()
-    print(batch_loss)
+    print('loss: {0}'.format(batch_loss))
 
 # training
 # for epoch in range(num_epochs):
